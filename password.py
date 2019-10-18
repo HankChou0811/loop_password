af81474ea82e560ec86b996ebd4b606c9e4a386e
Hank Chou <kkkabc7419631@hotmail.com>
2019-10-18 03:20:21 -0700

    third version !

--- password.py
+++ password.py
@@ -1,13 +1,14 @@
 password = 'a123456'
 i = 3
-while i > 0:
+while i >= 0:
+	i = i - 1
 	pwd = input('please enter the password:')
 	if pwd == password:
 		print('LOGIN IN!!')
 		break
-	else:
-		i = i - 1
+	elif i > 0:
 		print('wrong password,', i ,'times left')
-		
-
+	else:
+		print('wrong password, account automatic locks')
+		break
 
